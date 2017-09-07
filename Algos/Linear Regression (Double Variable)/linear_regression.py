f14850cfb11aa3c11535f6a2f230090477b92467
Shivam Saboo <201652025@iiitvadodara.ac.in>
2017-09-07 15:19:40 +0000

    Added Linear Regression in Python (#5)

---
+++ Algos/Linear Regression (Double Variable)/linear_regression.py
@@ -0,0 +1,74 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+datafile = 'data1'
+cols = np.loadtxt(datafile,delimiter=',',usecols=(0,1),unpack=True) #Read in comma separated data
+#Form the usual "X" matrix and "y" vector
+X = np.transpose(np.array(cols[:-1]))
+y = np.transpose(np.array(cols[-1:]))
+m = y.size # number of training examples
+#Insert the usual column of 1's into the "X" matrix
+X = np.insert(X,0,1,axis=1)
+
+plt.figure(figsize=(10,6))
+plt.plot(X[:,1],y[:,0],'rx',markersize=10)
+plt.grid(True) #Always plot.grid true!
+plt.ylabel('Profit in $10,000s')
+plt.xlabel('Population of City in 10,000s')
+
+iterations = 1500
+alpha = 0.01
+
+def h(theta,X): #Linear hypothesis function
+    return np.dot(X,theta)
+
+def computeCost(mytheta,X,y): #Cost function
+    """
+    theta_start is an n- dimensional vector of initial theta guess
+    X is matrix with n- columns and m- rows
+    y is a matrix with m- rows and 1 column
+    """
+    #note to self: *.shape is (rows, columns)
+    return float((1./(2*m)) * np.dot((h(mytheta,X)-y).T,(h(mytheta,X)-y)))
+
+#Test that running computeCost with 0's as theta returns 32.07:
+
+initial_theta = np.zeros((X.shape[1],1)) #(theta is a vector with n rows and 1 columns (if X has n features) )
+
+
+#Actual gradient descent minimizing routine
+def descendGradient(X, theta_start = np.zeros(2)):
+    """
+    theta_start is an n- dimensional vector of initial theta guess
+    X is matrix with n- columns and m- rows
+    """
+    theta = theta_start
+    jvec = [] #Used to plot cost as function of iteration
+    thetahistory = [] #Used to visualize the minimization path later on
+    for meaninglessvariable in range(iterations):
+        tmptheta = theta
+        jvec.append(computeCost(theta,X,y))
+        # Buggy line
+        #thetahistory.append(list(tmptheta))
+        # Fixed line
+        thetahistory.append(list(theta[:,0]))
+        #Simultaneously updating theta values
+        for j in range(len(tmptheta)):
+            tmptheta[j] = theta[j] - (alpha/m)*np.sum((h(initial_theta,X) - y)*np.array(X[:,j]).reshape(m,1))
+        theta = tmptheta
+    return theta, thetahistory, jvec
+
+initial_theta = np.zeros((X.shape[1],1))
+theta, thetahistory, jvec = descendGradient(X,initial_theta)
+
+#Plot the line on top of the data to ensure it looks correct
+def myfit(xval):
+    return theta[0] + theta[1]*xval
+plt.figure(figsize=(10,6))
+plt.plot(X[:,1],y[:,0],'rx',markersize=10,label='Training Data')
+plt.plot(X[:,1],myfit(X[:,1]),'b-',label = 'Hypothesis: h(x) = %0.2f + %0.2fx'%(theta[0],theta[1]))
+plt.grid(True) #Always plot.grid true!
+plt.ylabel('Profit in $10,000s')
+plt.xlabel('Population of City in 10,000s')
+plt.legend()
+plt.show()
