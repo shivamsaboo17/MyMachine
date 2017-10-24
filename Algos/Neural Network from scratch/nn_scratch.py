fc0f6cf1fb6cf18ff7fd7c45c0b135cec231a649
shivamsaboo17 <201652025@iiitvadodara.ac.in>
2017-10-24 19:04:47 +0530

    Neural Network from scratch

---
+++ Algos/Neural Network from scratch/nn_scratch.py
@@ -0,0 +1,188 @@
+import numpy as np
+import scipy.special
+import matplotlib.pyplot
+
+# neural network class defination
+class neuralNetwork:
+    # initialise the neural network
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+        # set number of nodes in each input, hidden, output layer
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+
+        # link weight matrices, wih and who
+        # weights inside the arrays are w_i_j, where link is from node i to node j in the next layer
+        # w11 w21
+        # w12 w22 etc 
+        self.wih = np.random.normal(0.0, pow(self.inodes, -0.5), (self.hnodes, self.inodes))
+        self.who = np.random.normal(0.0, pow(self.hnodes, -0.5), (self.onodes, self.hnodes))
+
+        # learning rate
+        self.lr = learningrate
+
+        # activation function is the sigmoid function
+        self.activation_function = lambda x: scipy.special.expit(x)
+        self.inverse_activation_function = lambda x: scipy.special.logit(x)
+        pass
+
+    # train the neural network
+    def train(self, inputs_list, targets_list):
+        # convert inputs list to 2d array
+        inputs = np.array(inputs_list, ndmin=2).T
+        targets = np.array(targets_list, ndmin=2).T
+
+        # calculate signals into hidden layer
+        hidden_inputs = np.dot(self.wih, inputs)
+        # calculate the signals emerging from hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        # calculate signals into final output layer
+        final_inputs = np.dot(self.who, hidden_outputs)
+        # calculate the signals emerging from final output layer
+        final_outputs = self.activation_function(final_inputs)
+
+        # output layer error is the (target - actual)
+        output_errors = targets - final_outputs
+        # hidden layer error is the output_errors, split by weights, recombined at hidden nodes
+        hidden_errors = np.dot(self.who.T, output_errors)
+
+        # update the weights for the links between the hidden and output layers
+        self.who += self.lr * np.dot((output_errors * final_outputs * (1.0 - final_outputs)),
+                                        np.transpose(hidden_outputs))
+
+        # update the weights for the links between the input and hidden layers
+        self.wih += self.lr * np.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)),
+                                        np.transpose(inputs))
+
+        pass
+
+    # query the neural network
+    def query(self, inputs_list):
+        # convert inputs list to 2d array
+        inputs = np.array(inputs_list, ndmin=2).T
+
+        # calculate signals into hidden layer
+        hidden_inputs = np.dot(self.wih, inputs)
+        # calculate the signals emerging from hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        # calculate signals into final output layer
+        final_inputs = np.dot(self.who, hidden_outputs)
+        # calculate the signals emerging from final output layer
+        final_outputs = self.activation_function(final_inputs)
+
+        return final_outputs
+
+    def backquery(self, targets_list):
+        # transpose the targets list to a vertical array
+        final_outputs = np.array(targets_list, ndmin=2).T
+
+        # calculate the signal into the final output layer
+        final_inputs = self.inverse_activation_function(final_outputs)
+
+        # calculate the signal out of the hidden layer
+        hidden_outputs = np.dot(self.who.T, final_inputs)
+        # scale them back to 0.01 to .99
+        hidden_outputs -= np.min(hidden_outputs)
+        hidden_outputs /= np.max(hidden_outputs)
+        hidden_outputs *= 0.98
+        hidden_outputs += 0.01
+
+        # calculate the signal into the hidden layer
+        hidden_inputs = self.inverse_activation_function(hidden_outputs)
+
+        # calculate the signal out of the input layer
+        inputs = np.dot(self.wih.T, hidden_inputs)
+        # scale them back to 0.01 to .99
+        inputs -= np.min(inputs)
+        inputs /= np.max(inputs)
+        inputs *= 0.98
+        inputs += 0.01
+
+        return inputs
+
+
+# Define all the variables for creating the class instance
+inputnodes = 784
+hiddennodes = 200
+outputnodes = 10
+
+learningrate = 0.1
+
+# create instance of the neural network class
+n = neuralNetwork(inputnodes, hiddennodes, outputnodes, learningrate)
+
+# Load the training data set (104 MB)
+print("Loading 60000 images...!")
+training_data_file = open("mnist_train.csv", 'r')
+training_data_list = training_data_file.readlines()
+training_data_file.close()
+print("Loaded the data!")
+print("Training the network...(May take upto 2-3 min as data set is > 100 MB)")
+
+# Run this loop 5 times to get > 97% accuracy (takes ~10 min to train)
+for record in training_data_list:
+        # split the record by the ',' commas
+        all_values = record.split(',')
+        # scale and shift the inputs
+        inputs = (np.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+        # create the target output values (all 0.01, except the desired label which is 0.99)
+        targets = np.zeros(outputnodes) + 0.01
+        # all_values[0] is the target label for this record
+        targets[int(all_values[0])] = 0.99
+        n.train(inputs, targets)
+        pass
+
+# Load the test data set (16 MB)
+test_data = open("mnist_test.csv")
+test_data_list = test_data.readlines()
+test_data.close()
+
+scorecard = []
+
+print("Training done...")
+print("Testing the network...")
+
+# go through all the records in the test data set and calculate correct score
+for record in test_data_list:
+    # split the record by the ',' commas
+    all_values = record.split(',')
+    # correct answer is first value
+    correct_label = int(all_values[0])
+    # scale and shift the inputs
+    inputs = (np.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+    # query the network
+    outputs = n.query(inputs)
+    # the index of the highest value corresponds to the label
+    label = np.argmax(outputs)
+    # append correct or incorrect to list
+    if (label == correct_label):
+        # network's answer matches correct answer, add 1 to scorecard
+        scorecard.append(1)
+    else:
+        # network's answer doesn't match correct answer, add 0 to scorecard
+        scorecard.append(0)
+        pass
+
+    pass
+
+scorecard_array = np.asarray(scorecard)
+# Print the accuracy of the model (~ 96%)
+print ("performance = ", scorecard_array.sum() / scorecard_array.size)
+print("Accuracy increases to > 97% if we train the model 5 times but training takes ~10 minutes to train")
+print("The tensorflow implementation of the same yeilds ~ 98% accuracy")
+
+label = 0
+# create the output signals for this label
+targets = np.zeros(outputnodes) + 0.01
+# all_values[0] is the target label for this record
+targets[label] = 0.99
+
+
+# get image data
+image_data = n.backquery(targets)
+
+# plot image data
+matplotlib.pyplot.imshow(image_data.reshape(28,28), cmap='Greys', interpolation='None')
+matplotlib.pyplot.show()
